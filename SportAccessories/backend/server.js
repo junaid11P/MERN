a9574c75b3b45cf37f9e97adca8da24b78c69eb1
Juned <79745275+junaid11P@users.noreply.github.com>
2025-05-25 21:09:33 +0530

    mongodb connected

--- SportAccessories/backend/server.js
+++ SportAccessories/backend/server.js
@@ -1,7 +1,23 @@
 const express= require('express');
+const  mongoose= require('mongoose');
 const PORT=3001;
 const app= express();
 
+mongoose.connect('mongodb://localhost:27017', {
+
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+app.get('/', (req, res) => {
+    try{
+        res.send('Hello World!');
+    }
+    catch(err){
+        console.error('Error in root route:', err);
+        res.status(500).send('Internal Server Error');
+    }
+    
+});
 app.listen(PORT, (err) => {
     if(err){
         console.error(`Error starting server: ${err}`);
