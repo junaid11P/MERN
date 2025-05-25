291eb0aae26f2bcdf32565a012331cb2870920e3
Juned <79745275+junaid11P@users.noreply.github.com>
2025-05-25 16:07:30 +0530

    footer added

--- SportAccessories/frontend/src/App.js
+++ SportAccessories/frontend/src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './component/Navbar';
+import Footer from './component/Footer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -9,13 +10,17 @@ import Register from './pages/Register';
 const App = () => {
   return (
     <Router>
-      <Navbar />
-      {/* Define your routes here */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
+      <div className="d-flex flex-column min-vh-100">
+        <Navbar />
+        <main className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </Router>
   );
 };
--- SportAccessories/frontend/src/component/Footer.jsx
+++ SportAccessories/frontend/src/component/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Footer = () => {
   return (
     <footer className="footer bg-dark text-light py-4 mt-auto">
