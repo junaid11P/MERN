a315c4425e2b175cf9be4f2f71f43a29a442bfa3
Juned <79745275+junaid11P@users.noreply.github.com>
2025-07-17 13:27:03 +0530

    New project

---
+++ basicreact/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* Navigation Menu */}
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link> |{" "}
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ basicreact/src/pages/Contact.js
@@ -0,0 +1,4 @@
+function Contact() {
+  return <h1>Get in touch on the Contact Page!</h1>;
+}
+export default Contact;
