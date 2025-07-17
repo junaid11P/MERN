e9e5d8a9cb867e034ec91b235ab566b1d587aad7
Juned <79745275+junaid11P@users.noreply.github.com>
2025-07-17 21:38:07 +0530

    open

--- aivoicebot/src/App.js
+++ aivoicebot/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/home';
+import Talk from './pages/talk';
 
 function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/talk" element={<Talk />} />
+        <Route path="*" element={<Navigate to="/" />} /> {/* Redirect unknown routes to Home */}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ aivoicebot/src/pages/talk.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const talk = () => {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundImage: "url('/assets/3ZSH.gif')",
+        backgroundSize: "cover",
+        backgroundColor: "black",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      {/* Your home page content here */}
+    </div>
+  );
+};
+
+export default talk;
